c7c7137bb925b3404755f8e660a2af12a56037ec
Corey <burkleycorey@yahoo.com>
2014-07-28 16:44:09 -0500

    Finished input and variable
    Created new Data Manipulation Directory

--- getting_input_and_using_variables/inputs_1-8.rb
+++ getting_input_and_using_variables/inputs_1-8.rb
@@ -25,3 +25,52 @@ puts "1 + 2 = #{1 + 2}"
 a = 2
 b = 4
 puts "2 + 4 = #{a + b}"
+# Output: 2 + 4 = 6
+#-------------------------------------------------------------
+
+# 4. Do the same as #2, but see what happens if one of the numbers is a floating point number (e.g. 4.2). What happens if both numbers are floating point numbers?
+a = 2.4
+b = 4
+puts "2.4 + 4 = #{a + b}"
+# Output: 2.4 + 4 = 6.4
+#-------------------------------------------------------------
+
+# 5. Research the different truth values: true, false, and nil. What does nil mean within Ruby?
+# The console will show the value of the last expression used in your code. If the last expression was empty or returned nil (the ruby object that represents "nothingness") you'll see ==> nil in the console.
+#-------------------------------------------------------------
+
+# 6. Use gets.chomp to collect input from the user and store it in a variable. Write a program that asks the user for their name, then print their name to the screen. Write a program that asks the user for their age, and then print that age to the screen.
+print "What is your first name?"
+first_name = gets.chomp
+first_name.capitalize!
+
+print "What is your last name?"
+last_name = gets.chomp
+last_name.capitalize!
+
+print "And how old are you?"
+how_old_are_you = gets.chomp
+how_old_are_you.to_s
+
+puts "/n Your name is #{first_name} #{last_name} and you are #{how_old_are_you} years of age!"
+# First your print command will ask the user for input and retain the information with gets.chomp after you have set the variable to whatever specificity.
+# .capitalize! - will capitalize the first letter of the given characters
+# This is another form of interpolation at work.
+#---------------------------------------------------------------
+
+# 7. gets.chomp collects input as Strings (text). Learn how to use gets.chomp.to_i to collect integers, as well as gets.chomp.to_f to collect floating point numbers. What happens if you try to call gets.chomp.to_i when a floating point number is entered? How about when a word or letter is entered? What happens if you use gets.chomp.to_f and input an integer? A word?
+
+print "What is your favorite number?"
+number = gets.chomp.to_i
+puts "\n #{number} is your favorite number!"
+
+# If the gets.chomp command is set to .to_i it will convert whatever data into a integer, so if you enter a letter or word,  it will return 0.
+# If you enter a float number such as 5.3, it will return you the first value or integer 5.
+#----------------------------------------------------------------
+
+# 8. What is the difference between puts 1 + 1 and puts "1" + "1"? Why does this behavior happen?
+
+# puts 1 + 1 will act upon taking the integers and adding them together and will output the sum.
+# puts "1" + "1" will recognize "1" as a string and will group the two strings together outputting 11
+#-----------------------------------------------------------------
+
