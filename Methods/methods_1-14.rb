cade21f7da021f0f7edf7766aba0c72c4ffe73f4
Corey <burkleycorey@yahoo.com>
2014-07-30 12:22:52 -0500

    Just begun the methods questions.

---
+++ Methods/methods_1-14.rb
@@ -0,0 +1,20 @@
+# 1. Learn how to create a method.
+def call
+  puts "This is my method"
+end
+call
+# Methods are used to bundle or repeate multiple statements into a single unit.
+# When creating a method you first must define the method by giving it a name or variable in order to call the method.
+# Sometimes you may need to give your method some parameters to abide by. If this is the case, your defined method would look like this: def call( p1, p2 )
+#---------------------------------------------------------
+
+# 2. Learn the meanings of each of these terms regarding methods:
+
+#    Method signature/name
+#    Parameters
+#    Arguments (how are parameters and arguments different?)
+#    Method call
+#    Return values
+#    Method body
+
+  
