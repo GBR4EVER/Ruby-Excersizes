c1882730782ddea501caab497de2946a6baa985f
Corey <burkleycorey@yahoo.com>
2014-08-01 11:36:29 -0500

    Functionality of tic tac toe operates smoothly. Never had to create loop for turn method. Just branched the it.

--- tic_tac-toe/game.rb
+++ tic_tac-toe/game.rb
@@ -1,4 +1,4 @@
-#require "pry"
+require "pry"
 
 class Game
   attr_reader :turn
@@ -8,49 +8,46 @@ class Game
     @board = { a1: " ", a2: " ", a3: " ", b1: " ", b2: " ", b3: " ", c1: " ", c2: " ", c3: " " }
   end
   
-  # def start_game
-  #   x = Game.new
-  #   x.intro
-  #   x.player_symbol
-  #   x.player_turn
-  #   x.check_game
-  # end
+   def start_game
+     x = Game.new
+     x.intro
+     x.player_symbol
+     x.player_turn
+   end
   
   def intro
-    puts "\t\t\t\t\t\t   ULTIMATE TIC TAC TOE .. in ruby!\n"
+    puts "ULTIMATE TIC TAC TOE .. in ruby!\n"
     display_board
-    puts " "
-    puts "\t\t\t Align three of your markers vertically, horizontally, or diagonally and your the winner!" 
-    puts "\t\tPlace your marker on the boarby inputting the designated spot of it's letter (row) and number (column)."
-    puts "\t\t\t\t\t\t               BEGIN!"
   end
   
   def player_symbol
-    puts "\t\t\t\t\t\tPlayer 1, choose your marker, X or O?\n"
+    puts "Player 1, choose your marker, X or O?\n"
     i = gets.chomp.upcase 
     if i == "X"
-      puts "\t\t\t\t\t\t Player 1 is X!"
-      puts "\t\t\t\t\t\t Player 2 is O!"
+      puts "Player 1 is X!"
+      puts "Player 2 is O!"
       @p1 = "X"
       @p2 = "O"
     elsif i == "O"
-      puts "\t\t\t\t\t\t Player 1 is 0!"
-      puts "\t\t\t\t\t\t Player 2 is X!"
+      puts "Player 1 is 0!"
+      puts "Player 2 is X!"
       @p1 = "O"
       @p2 = "X"
     else
-      puts "\t\t\t\t\t\tThat is not a valid player! Re-enter X or O!"
+      puts "That is not a valid player! Re-enter X or O!"
       player_symbol
     end
   end
 
   def player_turn
+    # Determine player turn.
+    # Pass to player_move.
     if @turn == 1
-      puts "\t\t\t\t\t\t Player 1 turn."
+      puts "Player 1 turn."
       @turn = 2
       player_move(@p1)
-    elsif
-      puts "\t\t\t\t\t\t Player 2 turn."
+    else
+      puts "Player 2 turn."
       @turn = 1
       player_move(@p2)
     end
@@ -58,25 +55,34 @@ class Game
 
   def display_board
     # Displays board grid using hash values.
-    puts "\t\t\t\t\t\t\t     1   2   3 "
-    puts "\t\t\t\t\t\t\t  A  #{@board[:a1]} | #{@board[:a2]} | #{@board[:a3]} "
-    puts "\t\t\t\t\t\t\t    ---+---+---"
-    puts "\t\t\t\t\t\t\t  B  #{@board[:b1]} | #{@board[:b2]} | #{@board[:b3]} "
-    puts "\t\t\t\t\t\t\t    ---+---+---"
-    puts "\t\t\t\t\t\t\t  C  #{@board[:c1]} | #{@board[:c2]} | #{@board[:c3]} "
+    puts "   1   2   3 "
+    puts "A  #{@board[:a1]} | #{@board[:a2]} | #{@board[:a3]} "
+    puts "  ---+---+---"
+    puts "B  #{@board[:b1]} | #{@board[:b2]} | #{@board[:b3]} "
+    puts "  ---+---+---"
+    puts "C  #{@board[:c1]} | #{@board[:c2]} | #{@board[:c3]} "
   end
 
   
   def player_move(n)
     # Player picks square to claim.
     # Player symbol populates hash.
-    puts "\t\t\t\t\t\t Pick move"
+    puts "Pick move #{n}"
     x = gets.chomp.downcase.to_sym
-    @board[x] = "#{n}" # Directs to player X/O
-    display_board
-    
+    if x == :a1 || x == :a2 || x == :a3 || 
+       x == :b1 || x == :b2 || x == :b3 || 
+       x == :c1 || x == :c2 || x == :c3
+      @board[x] = "#{n}" # Sets board space to @p1 or @p2
+      display_board
+      check_board
+    else
+      puts "That is not a valid option!"
+      puts "Please try again!"
+      player_move(n)
+    end
   end
-  def game_results
+  
+  def check_board
     # Determines winner, loser, or tie game
   if ((@board[:a1] == @p1) && (@board[:a2] == @p1) && (@board[:a3] == @p1)) ||
     ((@board[:b1] == @p1) && (@board[:b2] == @p1) && (@board[:b3] == @p1)) ||
@@ -87,27 +93,48 @@ class Game
     ((@board[:a1] == @p1) && (@board[:b2] == @p1) && (@board[:c3] == @p1)) ||
     ((@board[:a3] == @p1) && (@board[:b2] == @p1) && (@board[:c1] == @p1))
     puts "Player #{@p1} is the winner!"
-   elsif ((@board[:a1] == @p2) && (@board[:a2] == @p2) && (@board[:a3] == @p2)) ||
-     ((@board[:b1] == @p2) && (@board[:b2] == @p2) && (@board[:b3] == @p2)) ||
-     ((@board[:c1] == @p2) && (@board[:c2] == @p2) && (@board[:c3] == @p2)) ||
-     ((@board[:a1] == @p2) && (@board[:b1] == @p2) && (@board[:b1] == @p2)) ||
-     ((@board[:a2] == @p2) && (@board[:b2] == @p2) && (@board[:c2] == @p2)) ||
-     ((@board[:a3] == @p2) && (@board[:b3] == @p2) && (@board[:c3] == @p2)) ||
-     ((@board[:a1] == @p2) && (@board[:b2] == @p2) && (@board[:c3] == @p2)) ||
-     ((@board[:a3] == @p2) && (@board[:b2] == @p2) && (@board[:c1] == @p2))
-     puts "Player #{@p2} is the winner!"
-   elsif ((@board[:a1] == ("X" || "O")) &&
-     (@board[:a2] == ("X" || "O")) &&
-     (@board[:a3] == ("X" || "O")) &&
-     (@board[:b1] == ("X" || "O")) &&
-     (@board[:b2] == ("X" || "O")) &&
-     (@board[:b3] == ("X" || "O")) &&
-     (@board[:c1] == ("X" || "O")) &&
-     (@board[:c2] == ("X" || "O")) &&
-     (@board[:c3] == ("X" || "O")))
+    puts "Do you want to play again? Y or N?"
+    i = gets.chomp.upcase
+    if i == "Y"
+      start_game
+    elsif i == "N"
+      puts "Goodbye"
+    end
+    elsif ((@board[:a1] == @p2) && (@board[:a2] == @p2) && (@board[:a3] == @p2)) ||
+      ((@board[:b1] == @p2) && (@board[:b2] == @p2) && (@board[:b3] == @p2)) ||
+      ((@board[:c1] == @p2) && (@board[:c2] == @p2) && (@board[:c3] == @p2)) ||
+      ((@board[:a1] == @p2) && (@board[:b1] == @p2) && (@board[:b1] == @p2)) ||
+      ((@board[:a2] == @p2) && (@board[:b2] == @p2) && (@board[:c2] == @p2)) ||
+      ((@board[:a3] == @p2) && (@board[:b3] == @p2) && (@board[:c3] == @p2)) ||
+      ((@board[:a1] == @p2) && (@board[:b2] == @p2) && (@board[:c3] == @p2)) ||
+      ((@board[:a3] == @p2) && (@board[:b2] == @p2) && (@board[:c1] == @p2))
+      puts "Player #{@p2} is the winner!"
+      puts "Do you want to play again? Y or N?"
+      i = gets.chomp.upcase
+      if i == "Y"
+        start_game
+      elsif i == "N"
+        puts "Goodbye"
+      end
+   elsif (@board[:a1] == @p1 || @board[:a1] == @p2) &&
+      (@board[:a2] == @p1 || @board[:a2] == @p2) &&
+     (@board[:a3] == @p1 || @board[:a3] == @p2) &&
+     (@board[:b1] == @p1 || @board[:b1] == @p2) &&
+     (@board[:b2] == @p1 || @board[:b2] == @p2) &&
+     (@board[:b3] == @p1 || @board[:b3] == @p2) &&
+     (@board[:c1] == @p1 || @board[:c1] == @p2) &&
+     (@board[:c2] == @p1 || @board[:c2] == @p2) &&
+     (@board[:c3] == @p1 || @board[:c3] == @p2)
      puts "Tie Game"
-   else
-     player_turn
+     puts "Do you want to play again? Y or N?"
+     i = gets.chomp.upcase
+     if i == "Y"
+       start_game
+     elsif i == "N"
+       puts "Goodbye"
+     end
+    else
+      player_turn
    end
  end
       
