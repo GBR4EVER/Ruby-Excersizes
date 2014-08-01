c6d3f6ae8de5623e1c982c93b89b3fb7cd6905a3
Corey <burkleycorey@yahoo.com>
2014-08-01 11:39:56 -0500

    Probably should store certain methods within their own files. But not til later

--- tic_tac-toe/tic_tac_toe_test.rb
+++ tic_tac-toe/tic_tac_toe_test.rb
@@ -1,10 +1,10 @@
-require "pry"
+
 
 require_relative "./game.rb"
 
   new_game = Game.new
   new_game.intro
   new_game.player_symbol
-  new_game.player_turn # Must create loop. No user prompt after player 1 is asked and board is displayed.
+  new_game.player_turn
 
 
