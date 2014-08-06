66502a81500e31536977b6067f813fbadfebd037
Corey <burkleycorey@yahoo.com>
2014-08-06 09:59:45 -0500

    added a player name method instead of just setting the players marker to p1 or p2

--- tic_tac-toe/lib/game.rb
+++ tic_tac-toe/lib/game.rb
@@ -1,44 +1,59 @@
 #require "pry"
 class Game
   attr_reader :turn
+  attr_accessor :board
   
   def initialize
     @turn = 1
     @board = { a1: " ", a2: " ", a3: " ", b1: " ", b2: " ", b3: " ", c1: " ", c2: " ", c3: " " }
   end
   
-   # def start_game
-   #   x = Game.new
-   #   x.intro
-   #   x.player_symbol
-   #   x.player_turn
-   # end
+   def start_game
+     x = Game.new
+     x.intro
+     x.player_name
+     x.player_turn
+   end
+   # ADD A METHOD FOR PLAYER TO HAVE A NAME AND TRACK WHO WINS EACH GAME AND KEEP A RECORD FOR EACH PLAYER
+   # ADD A A.I. METHOD TO PLAY AGAINST
   
-  # def intro
- #    # Gives intro to the game that displays the title and board.
- #    puts "ULTIMATE TIC TAC TOE .. in ruby!\n"
- #    display_board
- #  end
-  
-  def player_symbol
-    # Determines Player 1s and Player 2s marker or symbol.
-    puts "Player 1, choose your marker, X or O?\n"
-    i = gets.chomp.upcase 
-    if i == "X"
-      puts "Player 1 is X!"
-      puts "Player 2 is O!"
-      @p1 = "X"
-      @p2 = "O"
-    elsif i == "O"
-      puts "Player 1 is 0!"
-      puts "Player 2 is X!"
-      @p1 = "O"
-      @p2 = "X"
-    else
-      puts "That is not a valid player! Re-enter X or O!"
-      player_symbol
-    end
+  def intro
+    # Gives intro to the game that displays the title and board.
+    puts "ULTIMATE TIC TAC TOE .. in ruby!\n"
+    display_board
   end
+  
+  def player_name
+    puts "Player 1, please enter your name.\n"
+    player1 = gets.chomp.upcase
+    puts "Welcome #{player1}, you are marker 'X'.\n"
+    @p1 = "X"
+    
+    puts "Player 2, please enter your name.\n"
+    player2 = gets.chomp.upcase
+    puts "Welcome #{player2}, you are marker 'O'.\n"
+    @p2 = "O"
+  end
+    
+  # def player_symbol
+  #   # Determines Player 1s and Player 2s marker or symbol.
+  #   puts "Player 1, choose your marker, X or O?\n"
+  #   i = gets.chomp.upcase
+  #   if i == "X"
+  #     puts "Player 1 is X!"
+  #     puts "Player 2 is O!"
+  #     @p1 = "X"
+  #     @p2 = "O"
+  #   elsif i == "O"
+  #     puts "Player 1 is 0!"
+  #     puts "Player 2 is X!"
+  #     @p1 = "O"
+  #     @p2 = "X"
+  #   else
+  #     puts "That is not a valid player! Re-enter X or O!"
+  #     player_symbol
+  #   end
+  # end
 
   def player_turn
     # Determine player turn.
@@ -85,7 +100,7 @@ class Game
   
   def check_board
     # Determines winner, loser, or tie game
-  if ((@board[:a1] == @p1) && (@board[:a2] == @p1) && (@board[:a3] == @p1)) ||
+    if ((@board[:a1] == @p1) && (@board[:a2] == @p1) && (@board[:a3] == @p1)) ||
     ((@board[:b1] == @p1) && (@board[:b2] == @p1) && (@board[:b3] == @p1)) ||
     ((@board[:c1] == @p1) && (@board[:c2] == @p1) && (@board[:c3] == @p1)) ||
     ((@board[:a1] == @p1) && (@board[:b1] == @p1) && (@board[:c1] == @p1)) ||
--- tic_tac-toe/spec/lib/game_spec.rb
+++ tic_tac-toe/spec/lib/game_spec.rb
@@ -4,8 +4,19 @@ require "board.rb"
 
 describe Game do
 
-  it "should check to see if turn is equal to 1 integer." do
-    i = Game.new(1)
-    expect(i.player_turn).to eql(1)
+  it "should check to see if of our init instances are set properly" do
+    game = Game.new
+    expect(game.turn).to eql(1)
+  end
+  
+  it "should init game board as hash" do
+    game = Game.new
+    board = { a1: " ", a2: " ", a3: " ", b1: " ", b2: " ", b3: " ", c1: " ", c2: " ", c3: " " }
+    expect(board).to eql(game.board)
+  end
+  
+  it "should give the intro of the game and display the empty board" do
+    game = Game.new
+    expect game.display_board.should_not == true
   end
 end
