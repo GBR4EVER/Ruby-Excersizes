9dd1f6083e9dab31b9d6c571d425282a73fdb889
Corey <burkleycorey@yahoo.com>
2014-08-06 15:57:52 -0500

    mod

--- movie_database/main.rb
+++ movie_database/main.rb
@@ -1,12 +1,12 @@
 require "pry"
 
 require "sinatra"
-require "sinatra_helper"
+require "sinatra/reloader"
 require "themoviedb"
 
 class Tmdb::Api.("3249ada114189590b51785edd4580c9d")
 
-
   binding.pry
+end
 
 
