0c714dc4c596f94eaf708cc96e09766a03384446
Corey <burkleycorey@yahoo.com>
2014-08-07 11:49:39 -0500

    mod

--- sinatra/main.rb
+++ sinatra/main.rb
@@ -3,8 +3,20 @@ require "pry"
 require "sinatra"
 require "sinatra/reloader"
 
+
+# When Sinatra's server recieves a request, the following happens:
+# 
+# 1.) First it recieves the request e.g. 'get "URL input" do finds the request and it must match:
+# 2.) The Verb ("get" or "post")
+# 3.) The Path (e.g "/hello")
+#
+# 4.) If it finds the path or match, Ruby code within that route's block.
+#
+# 5.) After finding the match, it runs the ruby code and displays the response.
+
+
 get "/" do
-  name = "Corey Burkley"
+  @names = ["ball", "puck", "frisbee"]
   
   erb :home # erb is taking the :home HTML file and embedding it.
 end
