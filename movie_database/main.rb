a5d9267c09d846dddcdcdf9ec92ea32993be283f
Corey <burkleycorey@yahoo.com>
2014-08-07 16:52:54 -0500

    mod

--- movie_database/main.rb
+++ movie_database/main.rb
@@ -3,7 +3,31 @@ require "pry"
 require "sinatra"
 require "sinatra/reloader"
 
+require "themoviedb"
+
 Tmdb::Api.key("3249ada114189590b51785edd4580c9d")
 
-binding.pry
+get "/" do
+  @now_playing = Tmdb::Movie.now_playing
+
+  erb :home
+end
+
+get "/search" do
+ @search = Tmdb::Movie.find(params["query"])
+ 
+ 
+binding.pry 
+erb :results
+
+end
+
+get "/movieid/:movieid" do
+  @movie = Tmdb::Movie.detail(params[:movieid])
+
+  
+
+  erb :movieid
+end
+
 
