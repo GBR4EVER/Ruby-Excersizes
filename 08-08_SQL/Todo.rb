f7217d79fd20180a44d5a3aa8d65283cf89ca6f0
Corey <burkleycorey@yahoo.com>
2014-08-11 12:03:51 -0500

    mod

--- 08-08_SQL/Todo.rb
+++ 08-08_SQL/Todo.rb
@@ -13,13 +13,13 @@ class Todo
     db = SQLite3::Database.new "Todo.db"
     db.results_as_hash = true
     
-    all_todos_from_db = db.execute(SELECT * FROM users)
+    all_todos_from_db = db.execute("SELECT * FROM users")
     
-    all_todos_from_db.each do |t_db|
-      id = |t_db|["id"]
-      home = |t_db|["home"]
-      work = |t_db|["work"]
-      estTime = |t_db|["estTime"]
+    all_todos_from_db.each do |u_db|
+      id = u_db["id"]
+      home = u_db["home"]
+      work = u_db["work"]
+      estTime = u_db["estTime"]
       
       all_todos_as_todo_objects << self.new(id, home, work, estTime)
     end
